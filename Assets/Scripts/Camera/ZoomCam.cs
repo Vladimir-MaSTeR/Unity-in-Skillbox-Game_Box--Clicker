a7ef19d315a7d2a0cd814082c567f2ccb0adbe8d
Vladimir Vladimirovich <tsyuman87binary@gmail.com>
2022-11-08 22:15:22 +0700

    initial commit

---
+++ Assets/Scripts/Camera/ZoomCam.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class ZoomCam : MonoBehaviour
+{
+    [SerializeField] private float zoomMin = 1;
+    [SerializeField] private float zoomMax = 8;
+
+    private void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
